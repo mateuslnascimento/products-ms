5b246e971ad702c3982b592e208e830e31fb68d0
Mateus Lopes do Nascimento <mateus-lopes@outlook.com>
2021-08-28 13:14:02 -0300

    Estrutura inicial da camada de service

---
+++ src/services/index.js
@@ -0,0 +1,10 @@
+import productsService from './productsService.js';
+
+export default (dependencies) => ({
+  postProducts: productsService(dependencies).putProducts,
+  getProducts: productsService(dependencies).getProducts,
+  putProducts: productsService(dependencies).putProducts,
+  patchProducts: productsService(dependencies).patchProducts,
+  deleteProducts: productsService(dependencies).deleteProducts,
+
+});
---
+++ src/services/productsService.js
@@ -0,0 +1,29 @@
+export default ({ mongo, config }) => {
+  const postProducts = async () => {
+
+  };
+
+  const getProducts = async () => {
+
+  };
+
+  const patchProducts = async () => {
+
+  };
+
+  const putProducts = async () => {
+
+  };
+
+  const deleteProducts = async () => {
+
+  };
+
+  return {
+    postProducts,
+    getProducts,
+    patchProducts,
+    putProducts,
+    deleteProducts,
+  };
+};
