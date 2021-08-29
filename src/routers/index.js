97ad099235d2e195531a7f163d1d3e2a76e5a57f
Mateus Lopes do Nascimento <mateus-lopes@outlook.com>
2021-08-29 01:29:55 -0300

    Estrutura inicial do arquivo de routers

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,3 +1,9 @@
 export default (express, controllers) => {
+  const { getProducts, postProducts } = controllers;
 
+  const router = express.Router();
+
+  router.get('/', getProducts);
+
+  router.post('/', postProducts);
 };
